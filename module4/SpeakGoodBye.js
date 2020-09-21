6446f88771c6e653a360f804aa575454621205da
Nikita-coding01 <66530225+Nikita-coding01@users.noreply.github.com>
2020-09-21 17:27:25 +0530

    Update SpeakGoodBye.js

--- module4/SpeakGoodBye.js
+++ module4/SpeakGoodBye.js
@@ -1,1 +1,13 @@
+(funtion { (window)
 
+var byeSpeaker = {}
+
+var speakWord = "Good Bye";
+
+byeSpeaker.speak function (name) {
+  console.log(speakWord + " " + name);
+}
+
+window.byeSpeaker = byeSpeaker
+
+}) (window);
